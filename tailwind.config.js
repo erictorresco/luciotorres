3106970a7a76d3f78bad37459fd837f317fc6ad2
erictorresco <erictorresco@gmail.com>
2021-05-07 12:20:53 -0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: [
+    './templates/**/*.twig',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
